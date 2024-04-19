87942bb058eb868d81994c1f6aa9785599430aed
Kyle Barker <kyle@thebarkers.net.nz>
2024-04-19 18:30:36 +1200

    Further comments to Node and StrBst

--- Node.java
+++ Node.java
@@ -1,8 +1,19 @@
-public class Node {
-    String value;
-    Node left;
-    Node right;
+/**
+ * Class to create nodes which are used in the implementation of a binary seach tree
+ * Each Node contains a value (string), and a pointer to the left and right nodes
+ */
 
+public class Node {
+    //value of the node
+    String value;
+    //pointer to the left node
+    Node left;
+    //pointer to the right node
+    Node right;
+    
+/**
+ * @param x the value to store in the node
+ */
     public Node(String x){
         value = x;
         left = null;
--- StrBST.java
+++ StrBST.java
@@ -1,17 +1,32 @@
+/**
+ * This class is performs operations including insertion, removal, searching and printing the Binary Search Tree
+ * @author Kyle Barker 
+ */
+
 public class StrBST {
     Node root;
 
     public StrBST() {
-
         root = null;
     }
 
+ /**
+  * Method used to insert into the BST
+  * @param s the value to insert
+  */
     public void insert(String s) {
         root = insertR(s, root);
     }
-
+/**
+ * Recursive method to insert a value to the BST
+ * @param s the value to add
+ * @param cRoot the head of the BST
+ * @return the root of the BST
+ */
     public Node insertR(String s, Node cRoot) {
-
+        //if head is null, tree is emtpy so insert at root
+        //checks if less than alphabetically, calls left
+        //else if greater than alphabetically, calls right
         if (cRoot == null) {
             cRoot = new Node(s);
         } else if (s.compareTo(cRoot.value) < 0) {
@@ -21,25 +36,30 @@ public class StrBST {
         }
         return cRoot;
     }
-
+/**
+ * Method to use to call recursive method to print tree
+ */
     public void print() {
         printR(root);
     }
-
+/**
+ * Recursive method to print tree using in order transveral
+ * @param cRoot
+ */
     public void printR(Node cRoot) {
+        //if head is null, tree is empty
         if (cRoot == null) {
             return;
         }
+        //transverse LHS
         printR(cRoot.left);
         System.out.print("Root: " + cRoot.value);
-
+        //process and print node
         if (cRoot.left == null) {
             System.out.print(" | Left: null");
         } else {
             System.out.print(" | Left: " + cRoot.left.value);
-
         }
-
         if (cRoot.right == null) {
             System.out.print(" | Right: null");
 
@@ -47,20 +67,35 @@ public class StrBST {
             System.out.print(" | Right: " + cRoot.right.value);
         }
         System.out.println();
-
-        
+        //transverse RHS
         printR(cRoot.right);
 
     }
-
+/**
+ * Method to call search function to find if a word exists in the tree
+ * @param s value to search for
+ * @return boolean if the value is contained in the tree
+ */
     public boolean search(String s) {
         boolean found = false;
+        //call recursive function parsing the value to search for and the root node
         found = searchR(s, root);
         return found;
     }
 
+    /**
+     * 
+     * @param s value to search for
+     * @param cRoot root of the BST
+     * @return boolean if BST contains value
+     */
     public boolean searchR(String s, Node cRoot) {
         boolean found = false;
+
+        //if the tree is empty, return false
+        //else if less than alaphabetically, search LHS
+        //else if greather than alpahbetically, search RHS
+        //else if equal, found
 
         if (cRoot == null) {
             return false;
@@ -102,12 +137,13 @@ public class StrBST {
         cRoot.right = removeR(cRoot.value, cRoot.right);
         return cRoot;
     }
-private String minString(Node cRoot){
-    String minString = cRoot.value;
-    while (cRoot.left !=null) {
-        minString = cRoot.left.value;
-        cRoot = cRoot.left;
+
+    private String minString(Node cRoot) {
+        String minString = cRoot.value;
+        while (cRoot.left != null) {
+            minString = cRoot.left.value;
+            cRoot = cRoot.left;
+        }
+        return minString;
     }
-    return minString;
-}
 }
