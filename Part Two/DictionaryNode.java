ee27643a4dac813e36126121f98e35e550b25133
Kyle Barker <kyle@thebarkers.net.nz>
2024-04-19 12:07:38 +1200

    Added comments

--- Part Two/DictionaryNode.java
+++ Part Two/DictionaryNode.java
@@ -1,9 +1,21 @@
-public class DictionaryNode {
-    String value;
-    String definition;
-    DictionaryNode left;
-    DictionaryNode right;
+/**
+ * Dictionary Class used to create a node of a dictionary BST that stores the value, the definition, and a pointer to the left and right node
+ */
 
+public class DictionaryNode {
+    //value of the node
+    String value;
+    //definition of the node
+    String definition;
+    //left child node
+    DictionaryNode left;
+    //right child node
+    DictionaryNode right;
+/**
+ * 
+ * @param x the value of the node
+ * @param y the definition of the node
+ */
     public DictionaryNode(String x, String y){
         value = x;
         definition = y;
